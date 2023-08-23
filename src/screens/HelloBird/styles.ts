b15719a67f3135c200e17056f2be228bb65eb8bc
illenafnayr <illenafnayr@gmail.com>
2023-08-22 23:43:54 -0400

    Hello Bird

---
+++ src/screens/HelloBird/styles.ts
@@ -0,0 +1,29 @@
+import {StyleSheet, ViewStyle, TextStyle} from 'react-native';
+
+interface Styles {
+  sectionContainer: ViewStyle;
+  sectionTitle: TextStyle;
+  sectionDescription: TextStyle;
+  highlight: TextStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default styles;
