95adfe0f09bbf5a555626ab7a86db81064ebb1bc
illenafnayr <illenafanayr@gmail.com>
2023-09-14 11:16:38 +0200

    spaces in brackets

--- src/screens/BirdRecordInput/BirdRecordInput.tsx
+++ src/screens/BirdRecordInput/BirdRecordInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import style from './style';
 
 export class BirdRecordInput extends React.Component<{}, {}> {
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import style from './style';
 import Logo from '../../common/logo/Logo';
-import {BirdRecordInput} from '../BirdRecordInput/BirdRecordInput';
+import { BirdRecordInput } from '../BirdRecordInput/BirdRecordInput';
 
 const Home: React.FC = () => {
   return (
